9f57c12a48aa45d62648dc7d8c779c5626036fa1
zhouxh-z <17826808007@163.com>
2021-09-14 16:10:49 +0800

    双指针

---
+++ src/main/java/algorithm/双指针/leetcode_15.java
@@ -0,0 +1,99 @@
+package algorithm.双指针;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 给你一个包含 n 个整数的数组nums，判断nums中是否存在三个元素 a，b，c ，使得a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+ *
+ * 注意：答案中不可以包含重复的三元组。
+ *
+ * 输入：nums = [-1,0,1,2,-1,-4]
+ * 输出：[[-1,-1,2],[-1,0,1]]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/3sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author zhouxh-z
+ * @date 2021/9/10 17:55
+ */
+public class leetcode_15 {
+
+
+    public List<List<Integer>> threeSum1(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        // 枚举 a
+        for (int first = 0; first < n; ++first) {
+            // 需要和上一次枚举的数不相同
+            if (first > 0 && nums[first] == nums[first - 1]) {
+                continue;
+            }
+            // c 对应的指针初始指向数组的最右端
+            int third = n - 1;
+            int target = -nums[first];
+            // 枚举 b
+            for (int second = first + 1; second < n; ++second) {
+                // 需要和上一次枚举的数不相同
+                if (second > first + 1 && nums[second] == nums[second - 1]) {
+                    continue;
+                }
+                // 需要保证 b 的指针在 c 的指针的左侧
+                while (second < third && nums[second] + nums[third] > target) {
+                    --third;
+                }
+                // 如果指针重合，随着 b 后续的增加
+                // 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+                if (second == third) {
+                    break;
+                }
+                if (nums[second] + nums[third] == target) {
+                    List<Integer> list = new ArrayList<Integer>();
+                    list.add(nums[first]);
+                    list.add(nums[second]);
+                    list.add(nums[third]);
+                    ans.add(list);
+                }
+            }
+        }
+        return ans;
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        final int length = nums.length;
+        if(length <3){
+            return null;
+        }
+        Arrays.sort(nums);
+        List<List<Integer>> rtn = new ArrayList<>();
+        int third ;
+        for (int i = 0; i < length; i++) {
+            if(i>0&&nums[i]==nums[i-1]){
+                continue;
+            }
+            third = length;
+            for (int j = i+1; j < length; j++) {
+                if(j>i+1&&nums[j]==nums[j-1]){
+                    continue;
+                }
+                for (int k = third-1; k >j; k--) {
+                    if(k<third-1&&nums[k]==nums[k+1]){
+                        continue;
+                    }
+                    if(nums[i]+nums[j]+nums[k] == 0){
+                        List<Integer> list = new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[k]);
+                        rtn.add(list);
+                        third = k;
+                        break;
+                    }
+                }
+            }
+        }
+        return rtn;
+    }
+}
